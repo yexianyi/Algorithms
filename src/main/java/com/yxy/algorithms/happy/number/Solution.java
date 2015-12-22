8b8a5e25dc03ead1e1f5a548d677a6eda8a70e72
Xianyi Ye <yexianyi@hotmail.com>
2015-12-22 15:53:35 +0800

    add algorithm - identify happy number

---
+++ src/main/java/com/yxy/algorithms/happy/number/Solution.java
@@ -0,0 +1,79 @@
+package com.yxy.algorithms.happy.number;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 	202. Happy Number My Submissions Question
+	Total Accepted: 46197 Total Submissions: 133330 Difficulty: Easy
+	Write an algorithm to determine if a number is "happy".
+	
+	A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+	
+	Example: 19 is a happy number
+	
+	12 + 92 = 82
+	82 + 22 = 68
+	62 + 82 = 100
+	12 + 02 + 02 = 1
+ * @author xianyiye
+ * 2015/12/22
+ */
+
+public class Solution {
+	
+	private static Set<Integer> resultSet = new HashSet<Integer>() ;
+	
+	public static boolean isHappy(int n) {
+		if(n==1){
+			return true ;
+		}
+		
+		if(resultSet.contains(n)){
+			return false ;
+		}
+		resultSet.add(n) ;
+		System.out.println(n);
+		
+        int sum =0 ;
+        while(n > 0){
+        	int remainder = n%10 ;
+        	sum += remainder * remainder ;
+        	n /= 10 ;
+        }
+        
+    	return isHappy(sum) ;
+        
+    }
+	
+	public boolean isHappy2(int n) {
+        if(n==1){
+			return true ;
+		}
+		
+		if(resultSet.contains(n)){
+			return false ;
+		}
+		resultSet.add(n) ;
+		
+        List<Integer> list = new ArrayList<Integer>() ;
+        while(n > 0){
+        	list.add(n%10) ;
+        	n /= 10 ;
+        }
+        
+    	n = 0 ;
+    	for (int num : list) {  
+    		n += num*num ; 
+    	}  
+    	return isHappy(n) ;
+    }
+
+	public static void main(String[] args) {
+		System.out.println(isHappy(1)) ;
+
+	}
+
+}
