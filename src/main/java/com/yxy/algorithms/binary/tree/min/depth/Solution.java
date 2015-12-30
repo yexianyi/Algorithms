17bb787d838912f80801a0cb0ab6465c80250784
Xianyi Ye <yexianyi@hotmail.com>
2015-12-30 15:01:29 +0800

    add algorithm - calculate min depth of binary tree

--- src/main/java/com/yxy/algorithms/binary/tree/min/depth/Solution.java
+++ src/main/java/com/yxy/algorithms/binary/tree/min/depth/Solution.java
@@ -3,14 +3,51 @@ package com.yxy.algorithms.binary.tree.min.depth;
 import com.yxy.algorithms.binary.tree.TreeNode;
 
 public class Solution {
+	private static int depth = -1 ;
 	
-	public int minDepth(TreeNode root) {
+	public static int minDepth(TreeNode root) {
+		int leftDepth = 0 ;
+    	int rightDepth = 0 ;
+    	
+    	if(root==null){
+    		return 0 ;
+    	}
+    	
+    	if(root.left!=null){
+    		leftDepth = 1 + minDepth(root.left) ;
+    	}
+    	if(root.right!=null){
+    		rightDepth = 1 + minDepth(root.right) ;
+    	}
+		
+        if(root.left==null && root.right==null){
+        	return 1 ;
+        }
         
-		return -1 ;
+    	if(leftDepth==0){
+    		return rightDepth ;
+    	}else if(rightDepth==0){
+    		return leftDepth ;
+    			
+    	}else if(leftDepth<=rightDepth){
+    		return leftDepth ;
+    	}else{
+    		return rightDepth ;
+    	}
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TreeNode root = new TreeNode(5) ;
+		root.left = new TreeNode(4)  ;
+		root.right = new TreeNode(8) ;
+		root.left.left = new TreeNode(11) ;
+		root.right.left = new TreeNode(13) ;
+		root.right.right = new TreeNode(4) ;
+//		root.left.left.left = new TreeNode(7) ;
+//		root.left.left.right = new TreeNode(2) ;
+		root.right.right.right = new TreeNode(1) ;
+		
+		System.out.println(minDepth(root));
 
 	}
 
