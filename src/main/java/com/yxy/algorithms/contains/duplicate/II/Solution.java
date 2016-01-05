ca8234996f436ffbb92ee8216ac3b9c205f3134c
Xianyi Ye <yexianyi@hotmail.com>
2016-01-05 17:09:04 +0800

    add algorithm - identify contains duplicate II

---
+++ src/main/java/com/yxy/algorithms/contains/duplicate/II/Solution.java
@@ -0,0 +1,21 @@
+package com.yxy.algorithms.contains.duplicate.II;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+	
+	public static boolean containsNearbyDuplicate(int[] nums, int k) {
+		
+
+		Set<Integer> set = new HashSet<Integer>() ;
+		
+		
+		return false;
+    }
+
+	public static void main(String[] args) {
+		System.out.println(containsNearbyDuplicate(new int[]{99,99}, 2));
+	}
+
+}
