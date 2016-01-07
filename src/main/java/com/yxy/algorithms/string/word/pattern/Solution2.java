70c7d84762eff423691c6a3dc33d72cc240259cd
Xianyi Ye <yexianyi@hotmail.com>
2016-01-07 15:30:08 +0800

    add algorithm - validate word pattern

---
+++ src/main/java/com/yxy/algorithms/string/word/pattern/Solution2.java
@@ -0,0 +1,60 @@
+package com.yxy.algorithms.string.word.pattern;
+
+/**
+ * 	290. Word Pattern My Submissions Question
+	Total Accepted: 20676 Total Submissions: 75398 Difficulty: Easy
+	Given a pattern and a string str, find if str follows the same pattern.
+	
+	Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+	
+	Examples:
+	pattern = "abba", str = "dog cat cat dog" should return true.
+	pattern = "abba", str = "dog cat cat fish" should return false.
+	pattern = "aaaa", str = "dog cat cat dog" should return false.
+	pattern = "abba", str = "dog dog dog dog" should return false.
+	Notes:
+	You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+ *
+ * @author xianyiye
+ * 2016/1/7
+ */
+
+public class Solution2 {
+	
+	public static boolean wordPattern(String pattern, String str) {
+		String[] array = str.split(" ") ;
+		if(pattern.length() != array.length){
+			return false ;
+		}
+		
+		String[] map = new String[26] ;
+		
+		for(int i=0; i<array.length; i++){
+			if(map[pattern.charAt(i)-96]==null){
+				//check if key has already existed
+				for(int j=0; j<26; j++){
+					if(map[j]!=null && map[j].equals(array[i])){
+						return false ;
+					}
+				}
+				map[pattern.charAt(i)-96]=array[i] ;
+			}else{
+				if(!map[pattern.charAt(i)-96].equals(array[i])){
+					return false ;
+				}
+			}
+		}
+		
+		return true;
+        
+    }
+
+	public static void main(String[] args) {
+		System.out.println(wordPattern("abba","dog cat cat dog"));//true
+		System.out.println(wordPattern("abba","dog cat cat fish"));//false
+		System.out.println(wordPattern("aaaa","dog cat cat dog"));//false
+		System.out.println(wordPattern("abba","dog dog dog dog"));//false
+
+	}
+
+}
