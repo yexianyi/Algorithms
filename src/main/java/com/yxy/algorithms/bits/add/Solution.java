59cda1c6a89c325ce157ee588b51851d1223e141
Xianyi Ye <yexianyi@hotmail.com>
2016-01-11 20:42:14 +0800

    update algorithm - binary add

--- src/main/java/com/yxy/algorithms/bits/add/Solution.java
+++ src/main/java/com/yxy/algorithms/bits/add/Solution.java
@@ -1,5 +1,27 @@
 package com.yxy.algorithms.bits.add;
 
+/**
+ * 
+	67. Add Binary
+	 My Submissions 
+	Question 
+	
+	Total Accepted: 69323 Total Submissions: 265819 Difficulty: Easy 
+	
+	
+	
+	
+	
+	Given two binary strings, return their sum (also a binary string). 
+	
+	For example,
+	 a = "11"
+	 b = "1"
+	 Return "100". 
+
+ * @author xianyiye
+ * 2016/1/11
+ */
 public class Solution {
 
 	public static String addBinary(String a, String b) {
@@ -14,19 +36,56 @@ public class Solution {
     	   array[i] = a.charAt(i) ;
        }
        
-       int i=0 ;
-       for(i=array.length-1; i>=0; i--){
-    	   if(array[i]=='1' && b.charAt(i)=='1'){//11
-    		   array[i] = 0 ;
-    		   array[i-1]++ ;
+       int increase = 0 ;
+       int i = 0 ;
+       int j = 0 ;
+       for(i=array.length-1 , j=b.length()-1; i>=0 && j>=0; i--,j--){
+    	   if(array[i]=='1' && b.charAt(j)=='1'){//	11
+    		   if(increase==1){
+    			   array[i] = '1' ;
+    		   }else{
+    			   array[i] = '0' ;
+    			   increase = 1 ;
+    		   }
+    	   }else if((array[i]=='1' ^ b.charAt(j)=='1')){// 10 or 01
+    		   if(increase==1){
+    			   increase = 1 ;
+    			   array[i] = '0' ;
+    		   }else{
+    			   array[i] = '1' ;
+    		   }
+    	   }else{//	00
+    		   if(increase==1){
+    			   increase = 0 ;
+    			   array[i] = '1' ;
+    		   }else{
+    			   array[i] = '0' ;
+    		   }
     	   }
        }
        
-		return null  ;
+       
+       if(increase==1){
+    	  while(i>=0 && array[i]=='1'){
+    		  array[i] = '0' ;
+    		  i-- ;
+    	  }
+    	  if(i>0){
+    		  array[i] = '1' ;
+    		  return new String(array) ;
+    	  }else{
+    		  return "1"+new String(array) ;
+    	  }
+       }
+       
+		return new String(array) ; 
     }
 
 	public static void main(String[] args) {
 		System.out.println(addBinary("0","0"));
+		System.out.println(addBinary("11","01"));
+		System.out.println(addBinary("1011","11"));
+		System.out.println(addBinary("11","1011"));
 
 	}
 
