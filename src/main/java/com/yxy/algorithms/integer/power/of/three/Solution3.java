d3f9a7516cc6fa9365153366b753076ad7d0e0e2
Xianyi Ye <yexianyi@hotmail.com>
2016-03-18 11:58:52 +0800

    add license headers

--- src/main/java/com/yxy/algorithms/integer/power/of/three/Solution3.java
+++ src/main/java/com/yxy/algorithms/integer/power/of/three/Solution3.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2016, Xianyi Ye
+ *
+ * This project includes software developed by Xianyi Ye
+ * yexianyi@hotmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.yxy.algorithms.integer.power.of.three;
 /**
  * 326. Power of Three My Submissions Question
