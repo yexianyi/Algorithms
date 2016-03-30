3d1f9bc6bab3b3b0813d1578e9e106b7148eb9eb
Xianyi Ye <yexianyi@hotmail.com>
2016-03-30 13:26:40 +0800

    add new algorithm about find insert position

---
+++ src/main/java/com/yxy/algorithms/sorted/array/search/insert/pos/Solution2.java
@@ -0,0 +1,43 @@
+package com.yxy.algorithms.sorted.array.search.insert.pos;
+
+import java.util.Random;
+
+/**
+ * 35. Search Insert Position My Submissions QuestionEditorial Solution Total
+ * Accepted: 99738 Total Submissions: 268948 Difficulty: Medium Given a sorted
+ * array and a target value, return the index if the target is found. If not,
+ * return the index where it would be if it were inserted in order.
+ * 
+ * You may assume no duplicates in the array.
+ * 
+ * Here are few examples. [1,3,5,6], 5 → 2 [1,3,5,6], 2 → 1 [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ * 
+ * 
+ * @author Xianyi Ye
+ * @Date 03/28/2016
+ */
+public class Solution2 {
+
+	public static int searchInsert(int[] nums, int target) {
+
+		if (nums.length == 0)
+			return 0;
+		int i = 0;
+		while (i < nums.length && nums[i] < target)
+			i++;
+		return i;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(searchInsert(new int[] { 1, 3 }, 2)); // 1
+		System.out.println(searchInsert(new int[] { 1, 3 }, 0)); // 0
+		System.out.println(searchInsert(new int[] { 3, 5, 7, 9, 10 }, 8)); // 3
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 5)); // 2
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 2)); // 1
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 7)); // 4
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 0)); // 0
+	}
+
+}
---
+++ src/main/java/com/yxy/algorithms/sorted/array/search/insert/pos/Solution3.java
@@ -0,0 +1,47 @@
+package com.yxy.algorithms.sorted.array.search.insert.pos;
+
+import java.util.Random;
+
+/**
+ * 35. Search Insert Position My Submissions QuestionEditorial Solution Total
+ * Accepted: 99738 Total Submissions: 268948 Difficulty: Medium Given a sorted
+ * array and a target value, return the index if the target is found. If not,
+ * return the index where it would be if it were inserted in order.
+ * 
+ * You may assume no duplicates in the array.
+ * 
+ * Here are few examples. [1,3,5,6], 5 → 2 [1,3,5,6], 2 → 1 [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ * 
+ * 
+ * @author Xianyi Ye
+ * @Date 03/28/2016
+ */
+public class Solution3 {
+
+	public static int searchInsert(int[] nums, int target) {
+		int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+
+            if (nums[mid] < target) l = mid + 1;
+            if (nums[mid] > target) r = mid - 1;
+        }
+
+        return l;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(searchInsert(new int[] { 1, 3 }, 2)); // 1
+		System.out.println(searchInsert(new int[] { 1, 3 }, 0)); // 0
+		System.out.println(searchInsert(new int[] { 3, 5, 7, 9, 10 }, 8)); // 3
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 5)); // 2
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 2)); // 1
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 7)); // 4
+		System.out.println(searchInsert(new int[] { 1, 3, 5, 6 }, 0)); // 0
+	}
+
+}
