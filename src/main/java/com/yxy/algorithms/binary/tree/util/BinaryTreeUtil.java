6081f20ae7ee74e8dccc392b3b27084933829d46
Xianyi Ye <yexianyi@hotmail.com>
2016-04-01 17:46:51 +0800

    submit temp code for TreeNode Library

---
+++ src/main/java/com/yxy/algorithms/binary/tree/util/BinaryTreeUtil.java
@@ -0,0 +1,18 @@
+package com.yxy.algorithms.binary.tree.util;
+
+import com.yxy.algorithms.binary.tree.TreeNode;
+
+public final class BinaryTreeUtil {
+	
+	public static TreeNode create(){
+		
+		
+		return null ;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
