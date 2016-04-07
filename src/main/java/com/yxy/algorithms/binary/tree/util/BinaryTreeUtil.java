0ca6205d8d14008d2b19fc593a23f9a58a5d9ae7
Xianyi Ye <yexianyi@hotmail.com>
2016-04-07 17:09:58 +0800

    enhance implementation on create binary tree

--- src/main/java/com/yxy/algorithms/binary/tree/util/BinaryTreeUtil.java
+++ src/main/java/com/yxy/algorithms/binary/tree/util/BinaryTreeUtil.java
@@ -4,14 +4,54 @@ import com.yxy.algorithms.binary.tree.TreeNode;
 
 public final class BinaryTreeUtil {
 	
-	public static TreeNode create(){
+	/**
+	 * Creates binary tree
+	 * @param node list
+	 */
+	public static TreeNode create(Integer[] nodes){
+		TreeNode[] nodeArray = new TreeNode[nodes.length] ;
+		for(int i=0; i<nodes.length; i++){
+			if(nodes[i]!=null){
+				nodeArray[i] = new TreeNode(nodes[i].intValue()) ; 
+			}
+		}
 		
+		int slow = 0 ;
+		int fast = 1 ;
 		
-		return null ;
+		TreeNode root = nodeArray[slow] ;
+		for(; fast<nodes.length; fast++){
+			if(fast%2 != 0){	//left
+				if(nodes[fast]!=null){
+					root.left = nodeArray[fast] ; 
+				}
+			}else{	//right
+				if(nodes[fast]!=null){
+					root.right = nodeArray[fast] ;
+				}
+				while(nodes[++slow]==null) ;
+				root = nodeArray[slow] ;
+			}
+		}
+		
+		return nodeArray[0] ;
 	}
+	
+	
+	/**
+	 * Print binary tree
+	 * @param root node
+	 */
+	public static void print(TreeNode root){
+		
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Integer[] nodes = {0,1,2,3,4,null,null,5} ;
+		TreeNode root = create(nodes) ;
+		
+		System.out.println(root);
 
 	}
 
