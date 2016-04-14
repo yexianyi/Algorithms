f86e79f14f0c3f95b0a21060100ce1cc9f12fded
Xianyi Ye <yexianyi@hotmail.com>
2016-04-14 16:04:37 +0800

    refine BinaryTreeUtil class in order to make it compatible with multi-types

---
+++ src/main/java/com/yxy/algorithms/binary/tree/TreeLinkNode.java
@@ -0,0 +1,14 @@
+package com.yxy.algorithms.binary.tree;
+
+public class TreeLinkNode {
+	public int val;
+	public TreeLinkNode left;
+	public TreeLinkNode right;
+	public TreeLinkNode next;
+
+	public TreeLinkNode(int x) {
+		this.val = x ;
+	}
+
+
+}
--- src/main/java/com/yxy/algorithms/binary/tree/util/BinaryTreeUtil.java
+++ src/main/java/com/yxy/algorithms/binary/tree/util/BinaryTreeUtil.java
@@ -1,9 +1,12 @@
 package com.yxy.algorithms.binary.tree.util;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+import com.yxy.algorithms.binary.tree.TreeLinkNode;
 import com.yxy.algorithms.binary.tree.TreeNode;
 
 public final class BinaryTreeUtil {
@@ -12,26 +15,44 @@ public final class BinaryTreeUtil {
 	 * Creates binary tree
 	 * @param node list
 	 */
-	public static TreeNode create(Integer[] nodes){
-		TreeNode[] nodeArray = new TreeNode[nodes.length] ;
+	public static <T> T create(Integer[] nodes, Class<T> theClass){
+		//TreeNode[] nodeArray = new TreeNode[nodes.length] ;
+		T[] nodeArray = (T[]) Array.newInstance(theClass, nodes.length) ;
 		for(int i=0; i<nodes.length; i++){
 			if(nodes[i]!=null){
-				nodeArray[i] = new TreeNode(nodes[i].intValue()) ; 
+				//nodeArray[i] = new TreeNode(nodes[i].intValue()) ; 
+				try {
+					nodeArray[i] = theClass.getDeclaredConstructor(Integer.TYPE).newInstance(nodes[i].intValue());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		
 		int slow = 0 ;
 		int fast = 1 ;
 		
-		TreeNode root = nodeArray[slow] ;
+		T root = nodeArray[slow] ;
 		for(; fast<nodes.length; fast++){
 			if(fast%2 != 0){	//left
 				if(nodes[fast]!=null){
-					root.left = nodeArray[fast] ; 
+					//root.left = nodeArray[fast] ; 
+					try {
+						Field lField = root.getClass().getField("left");
+						lField.set(root, nodeArray[fast]);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 			}else{	//right
 				if(nodes[fast]!=null){
-					root.right = nodeArray[fast] ;
+					//root.right = nodeArray[fast] ;
+					try {
+						Field rField = root.getClass().getField("right");
+						rField.set(root, nodeArray[fast]);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 				while(nodes[++slow]==null) ;
 				root = nodeArray[slow] ;
@@ -46,16 +67,16 @@ public final class BinaryTreeUtil {
 	 * Print binary tree
 	 * @param root node
 	 */
-	public static void print(TreeNode root){
-		Stack<List<TreeNode>> stack = new Stack<List<TreeNode>>() ;
-		List<TreeNode> list = new ArrayList<TreeNode>() ;
-		list.add(root) ;
+	public static <T> void print(T rootNode){
+		Stack<List<T>> stack = new Stack<List<T>>() ;
+		List<T> list = new ArrayList<T>() ;
+		list.add(rootNode) ;
 		stack.push(list) ;
 		
 		while(!stack.isEmpty()){
 			list = stack.pop() ;
 			boolean hasNotNullNode = false ;
-			for(TreeNode node : list){
+			for(T node : list){
 				if(node!=null){
 					hasNotNullNode = true ;
 					break ;
@@ -63,14 +84,23 @@ public final class BinaryTreeUtil {
 			}
 			
 			if(hasNotNullNode){
-				List<TreeNode> newList = new ArrayList<TreeNode>() ;
-				for(TreeNode node:list){
+				List<T> newList = new ArrayList<T>() ;
+				for(T node:list){
 					if(node==null){
 						System.out.print(" null");
 					}else{
-						System.out.print(" "+node.val);
-						newList.add(node.left) ;
-						newList.add(node.right) ;
+						try {
+							Field vfield = node.getClass().getField("val");
+							System.out.print(" "+vfield.getInt(node));
+							
+							Field lfield = node.getClass().getField("left");
+							newList.add((T) lfield.get(node)) ;
+							
+							Field rfield = node.getClass().getField("right");
+							newList.add((T) rfield.get(node)) ;
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 					}
 				}
 				stack.push(newList) ;
@@ -81,10 +111,12 @@ public final class BinaryTreeUtil {
 	
 
 //	public static void main(String[] args) {
-//		Integer[] nodes = {0} ;
-//		TreeNode root = create(nodes) ;
-//		
+//		Integer[] nodes = {0,1,2,3,4,5,6} ;
+//		TreeNode root = create(nodes, TreeNode.class) ;
 //		print(root);
+//		
+//		TreeLinkNode root2 = create(nodes, TreeLinkNode.class) ;
+//		print(root2);
 //
 //	}
 
