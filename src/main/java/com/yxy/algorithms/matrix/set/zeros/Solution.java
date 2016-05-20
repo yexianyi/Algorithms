739047a818328931105ee03c9133a7f89a076c89
Xianyi Ye <yexianyi@hotmail.com>
2016-05-20 16:47:55 +0800

    add comment

--- src/main/java/com/yxy/algorithms/matrix/set/zeros/Solution.java
+++ src/main/java/com/yxy/algorithms/matrix/set/zeros/Solution.java
@@ -2,7 +2,21 @@ package com.yxy.algorithms.matrix.set.zeros;
 
 import java.util.HashSet;
 import java.util.Set;
-
+/**
+ * 73. Set Matrix Zeroes My Submissions QuestionEditorial Solution
+	Total Accepted: 66514 Total Submissions: 198163 Difficulty: Medium
+	Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+	
+	click to show follow up.
+	
+	Follow up:
+	Did you use extra space?
+	A straight forward solution using O(mn) space is probably a bad idea.
+	A simple improvement uses O(m + n) space, but still not the best solution.
+	Could you devise a constant space solution?
+ * @author xianyiye
+ * @date 05/20/2016
+ */
 public class Solution {
 	
 	public static void setZeroes(int[][] matrix) {
