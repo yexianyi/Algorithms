1b3c19f25b65297bcf8ffed36363e25f5f19d788
Xianyi Ye <yexianyi@hotmail.com>
2016-05-24 17:01:52 +0800

    add algorithm to get H-index

---
+++ src/main/java/com/yxy/algorithms/array/h/index/Solution.java
@@ -0,0 +1,15 @@
+package com.yxy.algorithms.array.h.index;
+
+public class Solution {
+	
+	public static int hIndex(int[] citations) {
+		
+		
+		return -1 ;
+    }
+
+	public static void main(String[] args) {
+
+	}
+
+}
