eb8d318439045fba307c94753c836c4b15d77b91
Xianyi Ye <yexianyi@hotmail.com>
2016-05-25 13:29:00 +0800

    add comment

--- src/main/java/com/yxy/algorithms/array/rotated/sorted/II/Solution.java
+++ src/main/java/com/yxy/algorithms/array/rotated/sorted/II/Solution.java
@@ -1,5 +1,19 @@
 package com.yxy.algorithms.array.rotated.sorted.II;
 
+/**
+ * 81. Search in Rotated Sorted Array II My Submissions QuestionEditorial Solution
+	Total Accepted: 63429 Total Submissions: 198067 Difficulty: Medium
+	Follow up for "Search in Rotated Sorted Array":
+	What if duplicates are allowed?
+	
+	Would this affect the run-time complexity? How and why?
+	
+	Write a function to determine if a given target is in the array.
+
+ * @author xianyiye
+ * @date 05/24/2016
+ */
+
 public class Solution {
 	
 	public static boolean search(int[] nums, int target) {
