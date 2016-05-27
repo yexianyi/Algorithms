37ae03be2f3c881be683549b06ddfc5ac482df93
Xianyi Ye <yexianyi@hotmail.com>
2016-05-27 10:51:04 +0800

    add new algorithm to detect cycle II

--- src/main/java/com/yxy/algorithms/linked/list/detect/cycle/II/Solution.java
+++ src/main/java/com/yxy/algorithms/linked/list/detect/cycle/II/Solution.java
@@ -1,7 +1,21 @@
 package com.yxy.algorithms.linked.list.detect.cycle.II;
 
 import com.yxy.algorithms.linked.list.ListNode;
-
+/**
+ * 142. Linked List Cycle II My Submissions QuestionEditorial Solution
+	Total Accepted: 76275 Total Submissions: 243246 Difficulty: Medium
+	Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+	
+	Note: Do not modify the linked list.
+	
+	Follow up:
+	Can you solve it without using extra space?
+ * @author xianyiye
+ * @date 05/26/2016
+ * 
+ * Reference:
+ * https://leetcode.com/discuss/65724/concise-java-solution-based-on-slow-fast-pointers
+ */
 public class Solution {
 	public static ListNode detectCycle(ListNode head) {
 		if(head==null||head.next==null)
