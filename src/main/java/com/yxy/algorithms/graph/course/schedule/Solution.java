027bb550ec8f211773ecfca553f7a8b7d45318e9
Xianyi Ye <yexianyi@hotmail.com>
2016-07-21 21:59:39 +0800

    update algorithm to identify course schedule

--- src/main/java/com/yxy/algorithms/graph/course/schedule/Solution.java
+++ src/main/java/com/yxy/algorithms/graph/course/schedule/Solution.java
@@ -1,26 +1,61 @@
 package com.yxy.algorithms.graph.course.schedule;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution {
 	
-	public boolean canFinish(int numCourses, int[][] prerequisites) {
+	//timeout
+	public static boolean canFinish(int numCourses, int[][] prerequisites) {
 		Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>() ;
 		
-		for(int[] list : prerequisites){
-			
+		//init graph
+		for(int[] prerequisite : prerequisites){
+			int key = prerequisite[0] ;
+			int preCourse = prerequisite[1] ;
+			if(graph.containsKey(key)){
+				graph.get(key).add(preCourse) ;
+			}else{
+				List<Integer> preList = new ArrayList<Integer>() ;
+				preList.add(preCourse) ;
+				graph.put(key, preList) ;
+			}
 		}
 		
+		for(int i=0; i<numCourses; i++){
+			if(!dfsTravel(graph, i, new ArrayList<Integer>())){
+				return false ;
+			}
+		}
 		
+		return true ;
+	}
+
+	private static boolean dfsTravel(Map<Integer, List<Integer>> graph, int course, List<Integer> path) {
+		if(path.contains(course)){
+			return false ;
+		}
 		
-		return false;
+		path.add(course) ;
+		List<Integer> preCourses = graph.get(course) ;
+		if(preCourses!=null){
+			for(Integer preCourse : preCourses){
+				if(!dfsTravel(graph, preCourse, path)){
+					return false ;
+				}
+			}
+		}
+		path.remove(path.size()-1) ;
+		return true;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] prerequisites = {{0,1},{0,2},{1,2}} ;
+		System.out.println(canFinish(3, prerequisites)) ;
 	}
 
 }
