0b209d094abdffe6a5b2e188f0092cb0d581a800
Xianyi Ye <yexianyi@hotmail.com>
2016-07-21 17:15:10 +0800

    submit code to identify course schedule

---
+++ src/main/java/com/yxy/algorithms/graph/course/schedule/Solution.java
@@ -0,0 +1,26 @@
+package com.yxy.algorithms.graph.course.schedule;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+	
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>() ;
+		
+		for(int[] list : prerequisites){
+			
+		}
+		
+		
+		
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
