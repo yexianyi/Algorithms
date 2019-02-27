b7e2b16580722004ef92f48fe22b031f08c815b2
yexianyi <yexianyi@baidu.com>
2019-02-27 19:15:28 +0800

    Update algorithm of Buddy Strings

--- src/main/java/com/yxy/algorithms/string/buddy/strings/Solution.java
+++ src/main/java/com/yxy/algorithms/string/buddy/strings/Solution.java
@@ -1,46 +1,97 @@
 package com.yxy.algorithms.string.buddy.strings;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+/**
+ * 859. Buddy Strings
+Easy
+
+Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+Example 1:
+
+Input: A = "ab", B = "ba"
+Output: true
+Example 2:
+
+Input: A = "ab", B = "ab"
+Output: false
+Example 3:
+
+Input: A = "aa", B = "aa"
+Output: true
+Example 4:
+
+Input: A = "aaaaaaabc", B = "aaaaaaacb"
+Output: true
+Example 5:
+
+Input: A = "", B = "aa"
+Output: false
+ 
+
+Note:
+
+0 <= A.length <= 20000
+0 <= B.length <= 20000
+A and B consist only of lowercase letters.
+
+ * @author yexianyi
+ * 2019/02/26
+ */
 public class Solution {
-    
+
     public static boolean buddyStrings(String A, String B) {
-        
-        if(A.length() != B.length() || A.length() == 0 || B.length() == 0) {
-            return false ;
+        // If length is not equal with each other, return false ;
+        if (A.length() != B.length() || A.length() == 0 || B.length() == 0) {
+            return false;
         }
-        
-        char _chA1 =0 ,_chA2 = 0,_chB1 = 0,_chB2 = 0 ;
-        
-        for(int i=0; i<A.length() - 1; i++) {
-            _chA1 = A.charAt(i) ;
-            _chA2 = A.charAt(i+1) ;
-            _chB1 = B.charAt(i) ;
-            _chB2 = B.charAt(i+1) ;
-            
-            if(_chA1 == _chB1) {
-                continue ;
-            }
-            
-            if(_chA1 != _chB2 || _chA2 != _chB1) {
-                return false ;
+
+        // remember diff pos in two string
+        List<Integer> diffPos = new ArrayList<>(2);
+        int j = 0;
+
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt(i)) {
+                diffPos.add(i);
             }
         }
-        
-        if(_chA1 != _chB2 || _chA2 != _chB1) {
-            return false ;
+
+        // String A != String B
+        if (diffPos.size() == 2) {
+            if (A.charAt(diffPos.get(0)) == B.charAt(diffPos.get(1))
+             && A.charAt(diffPos.get(1)) == B.charAt(diffPos.get(0))) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        return true ;
+
+        // String A == String B && exists duplicate letters in A and B, then return true;
+        if (diffPos.size() == 0) {
+            Set<Character> diff = new HashSet<>(A.length());
+            for (char ch : A.toCharArray()) {
+                diff.add(ch);
+            }
+            if (diff.size() < A.length()) {
+                return true;
+            }
+        }
+
+        // String A and B constains different letter more than 2
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(buddyStrings("abab", "abab")) ;
-        System.out.println(buddyStrings("ab", "ba")) ;
-        System.out.println(buddyStrings("ab", "ab")) ;
-        System.out.println(buddyStrings("aa", "aa")) ;
-        System.out.println(buddyStrings("aaaaaaabc", "aaaaaaacb")) ;
-        
+        System.out.println(buddyStrings("abcaa", "abcbb")); // false
+        System.out.println(buddyStrings("aabc", "aacb")); // true
+        System.out.println(buddyStrings("ab", "ab")); // false
+        System.out.println(buddyStrings("ab", "ba")); // true
+        System.out.println(buddyStrings("aa", "aa")); // true
+        System.out.println(buddyStrings("abab", "abab")); // true
+
     }
 
 }
