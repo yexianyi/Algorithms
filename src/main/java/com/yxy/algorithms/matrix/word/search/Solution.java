4a69f5fcc4f996afbd92d2424b4eed7152d90743
yexianyi <yexianyi@baidu.com>
2019-08-12 10:27:45 +0800

    Update algorithm for Word Search

--- src/main/java/com/yxy/algorithms/matrix/word/search/Solution.java
+++ src/main/java/com/yxy/algorithms/matrix/word/search/Solution.java
@@ -1,19 +1,38 @@
 package com.yxy.algorithms.matrix.word.search;
 
-import java.util.HashSet;
-import java.util.Set;
-
+/**
+ * 79. Word Search
+    Given a 2D board and a word, find if the word exists in the grid.
+    
+    The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+    
+    Example:
+    
+    board =
+    [
+      ['A','B','C','E'],
+      ['S','F','C','S'],
+      ['A','D','E','E']
+    ]
+    
+    Given word = "ABCCED", return true.
+    Given word = "SEE", return true.
+    Given word = "ABCB", return false.
+ * @author yexianyi
+ * 2019/08/12
+ *
+ */
 public class Solution {
 
-    // Time Limit Exceeded
     public static boolean exist(char[][] board, String word) {
-        Set<String> path = new HashSet<>(word.length()) ;
-        
         for(int i=0; i<board.length; i++) {
             for(int j=0; j<board[0].length; j++) {
-                if(search(board, i, j, word, 0, path) == true){
-                    return true ;
+                if(board[i][j] == word.charAt(0)) {
+                    if(search(board, i, j, word, 0)){
+                        return true ;
+                    }
                 }
+               
             }
         }
         
@@ -21,32 +40,27 @@ public class Solution {
             
     }
     
-    public static boolean search(char[][] board, int row, int col, String word, int curr, Set<String> path) {
-        String pos = row + "" + col ;
-        if(row < 0 || row > board.length-1 || col < 0 || col > board[0].length-1 || curr == word.length() || path.contains(pos)) {
+    public static boolean search(char[][] board, int row, int col, String word, int curr) {
+        if(row < 0 || row == board.length || col < 0 || col == board[0].length || board[row][col]!=word.charAt(curr)) {
             return false ;
         }
         
-        path.add(pos) ;
+        char backup = board[row][col] ;
+        board[row][col] = ' ' ;
         
-        if(board[row][col] == word.charAt(curr)) {
-            if(curr == word.length()-1) {
-                return true ;
-            }
-            curr++ ;
-        } 
-        
-        
-        if(search(board, row + 1, col, word, curr, path)
-        || search(board, row - 1, col, word, curr, path)
-        || search(board, row, col + 1, word, curr, path)
-        || search(board, row, col - 1, word, curr, path)) {
+        if(curr == word.length()-1) {
             return true ;
         }
         
-        path.remove(pos) ;
         
-        return false ;
+        boolean res = search(board, row + 1, col, word, curr+1)
+                    || search(board, row - 1, col, word, curr+1)
+                    || search(board, row, col + 1, word, curr+1)
+                    || search(board, row, col - 1, word, curr+1) ;
+        
+        board[row][col] = backup ;
+        
+        return res ;
     }
 
     public static void main(String[] args) {
