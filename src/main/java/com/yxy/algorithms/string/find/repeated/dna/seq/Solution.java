6b179970cfa76472ee595975258c505e346278f0
z0044bpw <xianyi.ye@siemens.com>
2020-01-21 15:22:39 +0800

    update algorithm for Repeated DNA Sequences

--- src/main/java/com/yxy/algorithms/string/find/repeated/dna/seq/Solution.java
+++ src/main/java/com/yxy/algorithms/string/find/repeated/dna/seq/Solution.java
@@ -5,14 +5,29 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 187. Repeated DNA Sequences
+	Medium
+	All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+	
+	Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+	
+	Example:
+	
+	Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+	
+	Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+ * @author Ye Xianyi
+ * 2020/01/21
+ */
 public class Solution {
 
 	public static List<String> findRepeatedDnaSequences(String s) {
 		Set<String> set = new HashSet<>() ;
-		List<String> res = new ArrayList<>() ;
+		Set<String> res = new HashSet<>() ;
 		
 		int len = s.length() ;
-		for(int i=0; i+10<len; i++) {
+		for(int i=0; i+10<=len; i++) {
 			String subStr = s.substring(i, i+10) ;
 			if(set.contains(subStr)) {
 				res.add(subStr) ;
@@ -21,11 +36,12 @@ public class Solution {
 			}
 		}
 		
-		return res ;
+		return new ArrayList<>(res) ;
 	}
 
 	public static void main(String[] args) {
-		List<String> res = findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT") ;
+//		List<String> res = findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT") ;
+		List<String> res = findRepeatedDnaSequences("AAAAAAAAAAA") ;
 		for(String str:res) {
 			System.out.println(str);
 		}
