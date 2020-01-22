6f6648eaa6d37a9f76a998502aa0945e20a087a8
z0044bpw <xianyi.ye@siemens.com>
2020-01-22 14:35:26 +0800

    update algorithm for 207. Course Schedule

--- src/main/java/com/yxy/algorithms/graph/course/schedule/Solution2.java
+++ src/main/java/com/yxy/algorithms/graph/course/schedule/Solution2.java
@@ -1,52 +1,82 @@
 package com.yxy.algorithms.graph.course.schedule;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-public class Solution2 {
+/**
+ * 207. Course Schedule
+	Medium
+	There are a total of n courses you have to take, labeled from 0 to n-1.
 	
-	//kahn
-	public static boolean canFinish(int numCourses, int[][] prerequisites) {
-		int[][] graph = new int[numCourses][numCourses] ;
-		
-		//init graph
-		for(int[] prerequisite : prerequisites){
-			int course = prerequisite[0] ;
-			int preCourse = prerequisite[1] ;
-			graph[course][preCourse]++ ;
-		}
-		
-		
-		
-		
-		return true ;
-	}
+	Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+	
+	Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+	
+	Example 1:
+	
+	Input: 2, [[1,0]] 
+	Output: true
+	Explanation: There are a total of 2 courses to take. 
+	             To take course 1 you should have finished course 0. So it is possible.
+	Example 2:
+	
+	Input: 2, [[1,0],[0,1]]
+	Output: false
+	Explanation: There are a total of 2 courses to take. 
+	             To take course 1 you should have finished course 0, and to take course 0 you should
+	             also have finished course 1. So it is impossible.
+	Note:
+	
+	The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+	You may assume that there are no duplicate edges in the input prerequisites.
+ * @author Ye Xianyi
+ * 2020/01/22
+ * 
+ * Copy from:
+ * https://leetcode.com/problems/course-schedule/discuss/480820/Simple-Java-DFS
+ */
+public class Solution2 {
 
-	private static boolean dfsTravel(Map<Integer, List<Integer>> graph, int course, List<Integer> path) {
-		if(path.contains(course)){
-			return false ;
+	public static boolean canFinish(int numCourses, int[][] prerequisites) {
+		List<Integer>[] graph = new ArrayList[numCourses];
+		for (int i = 0; i < numCourses; i++) {
+			graph[i] = new ArrayList<>();
 		}
 		
-		path.add(course) ;
-		List<Integer> preCourses = graph.get(course) ;
-		if(preCourses!=null){
-			for(Integer preCourse : preCourses){
-				if(!dfsTravel(graph, preCourse, path)){
-					return false ;
-				}
-			}
+		for (int i = 0; i < prerequisites.length; i++) {
+			graph[prerequisites[i][1]].add(prerequisites[i][0]);
 		}
-		path.remove(path.size()-1) ;
+		
+		boolean[] restoredStack = new boolean[numCourses];
+		boolean[] visited = new boolean[numCourses];
+		for (int i = 0; i < numCourses; i++) {
+			if (visited[i])
+				continue;
+			if (isCycle(visited, restoredStack, i, graph))
+				return false;
+		}
 		return true;
 	}
 
+	private static boolean isCycle(boolean[] visited, boolean[] restoredStack, int idx, List<Integer>[] graph) {
+		if (restoredStack[idx])
+			return true;
+		if (visited[idx])
+			return false;
+		visited[idx] = true;
+		restoredStack[idx] = true;
+		for (int i = 0; i < graph[idx].size(); i++) {
+			if (isCycle(visited, restoredStack, graph[idx].get(i), graph))
+				return true;
+		}
+		restoredStack[idx] = false;
+		return false;
+	}
+
 	public static void main(String[] args) {
-		int[][] prerequisites = {{0,1},{0,2},{1,2}} ;
-		System.out.println(canFinish(3, prerequisites)) ;
+//		int[][] prerequisites = {{0,1},{0,2},{1,2}} ;
+		int[][] prerequisites = { { 1, 3 }, { 3, 4 }, { 4, 2 }, { 2, 1 }, { 5, 3 } };
+		System.out.println(canFinish(3, prerequisites));
 	}
 
 }
