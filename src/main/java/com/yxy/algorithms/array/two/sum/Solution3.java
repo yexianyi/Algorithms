7eabe4c60788f18bd652e6bd10014428bad0f02e
z0044bpw <xianyi.ye@siemens.com>
2020-08-07 13:15:15 +0800

    add new algorithm for 167. Tow Sum II

--- src/main/java/com/yxy/algorithms/array/two/sum/Solution3.java
+++ src/main/java/com/yxy/algorithms/array/two/sum/Solution3.java
@@ -16,34 +16,29 @@ import java.util.Map;
  * 
  * 
  * @author yexianyi 2019/01/17
+ * Copy from:
+ * https://leetcode.com/problems/two-sum/solution/
  */
 public class Solution3 {
 
     /**
-     * Approach 2: Two-pass Hash Table To improve our run time complexity, we need a more efficient way to check if the
-     * complement exists in the array. If the complement exists, we need to look up its index. What is the best way to
-     * maintain a mapping of each element in the array to its index? A hash table.
+     * Approach 3: One-pass Hash Table It turns out we can do it in one-pass. While we iterate and inserting elements
+     * into the table, we also look back to check if current element's complement already exists in the table. If it
+     * exists, we have found a solution and return immediately.
      * 
-     * We reduce the look up time from O(n)O(n) to O(1)O(1) by trading space for speed. A hash table is built exactly
-     * for this purpose, it supports fast look up in near constant time. I say "near" because if a collision occurred, a
-     * look up could degenerate to O(n)O(n) time. But look up in hash table should be amortized O(1)O(1) time as long as
-     * the hash function was chosen carefully.
+     * Runtime: 3 ms, faster than 64.62% of Java online submissions for Two Sum.
      * 
-     * A simple implementation uses two iterations. In the first iteration, we add each element's value and its index to
-     * the table. Then, in the second iteration we check if each element's complement (target - nums[i]target−nums[i])
-     * exists in the table. Beware that the complement must not be nums[i]nums[i] itself!
-     * 
+     * Time complexity : O(n)O(n). We traverse the list containing nn elements only once. Each look up in the table costs only O(1)O(1) time.
+       Space complexity : O(n)O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
      */
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[] { i, map.get(complement) };
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
