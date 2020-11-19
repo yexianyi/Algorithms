3699dea4a8740a13126ab0459a39b624ae5b7a9e
z0044bpw <xianyi.ye@siemens.com>
2020-11-19 17:23:57 +0800

    update algorithm for 713. Subarray Product Less Than K

--- src/main/java/com/yxy/algorithms/array/subarray/product/less/than/k/Solution2.java
+++ src/main/java/com/yxy/algorithms/array/subarray/product/less/than/k/Solution2.java
@@ -1,22 +1,51 @@
 package com.yxy.algorithms.array.subarray.product.less.than.k;
 
+/**
+ * 713. Subarray Product Less Than K
+    Medium
+    Your are given an array of positive integers nums.
+    
+    Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+    
+    Example 1:
+    Input: nums = [10, 5, 2, 6], k = 100
+    Output: 8
+    Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
+    Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+    Note:
+    
+    0 < nums.length <= 50000.
+    0 < nums[i] < 1000.
+    0 <= k < 10^6.
+ * @author Ye Xianyi
+ * 2020/11/19
+ * 
+ * Reference:
+ * https://leetcode.com/problems/subarray-product-less-than-k/
+ *
+ */
 public class Solution2 {
 
-    // Time Limit Exceeded
     public static int numSubarrayProductLessThanK(int[] nums, int k) {
-        int ans = 0;
-        
-        for(int i=0; i<nums.length; i++) {
-            int product = 1 ;
-            for(int j=i; j<nums.length; j++) {
-                product = product * nums[j] ;
-                if(product < k) {
-                    ans ++ ;
-                } else {
-                    break ;
-                }
-            }
+        if (k <= 1) {
+            return 0;
         }
+        
+        int ans = 0;
+        int accuProdcut = 1 ;
+        int left = 0 ;
+        
+        for(int right=0; right<nums.length; right++) {
+            accuProdcut *= nums[right] ;
+            while(left <= right && accuProdcut >= k) {
+                accuProdcut = accuProdcut / nums[left] ;
+                left ++ ;
+            }
+            
+            ans += right - left + 1;
+        }
+        
+        
         return ans ;
     }
 
