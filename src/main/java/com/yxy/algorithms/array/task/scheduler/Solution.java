fcc34485465e6ffd8bde16ea232d811933a358a9
z0044bpw <xianyi.ye@siemens.com>
2020-12-09 22:34:51 +0800

    add new algorithm for 621. Task Scheduler

---
+++ src/main/java/com/yxy/algorithms/array/task/scheduler/Solution.java
@@ -0,0 +1,83 @@
+package com.yxy.algorithms.array.task.scheduler;
+
+import java.util.Arrays;
+
+/**
+ * 621. Task Scheduler
+    Medium
+    Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+    
+    However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+    
+    Return the least number of units of times that the CPU will take to finish all the given tasks.
+    
+    Example 1:
+    
+    Input: tasks = ["A","A","A","B","B","B"], n = 2
+    Output: 8
+    Explanation: 
+    A -> B -> idle -> A -> B -> idle -> A -> B
+    There is at least 2 units of time between any two same tasks.
+    Example 2:
+    
+    Input: tasks = ["A","A","A","B","B","B"], n = 0
+    Output: 6
+    Explanation: On this case any permutation of size 6 would work since n = 0.
+    ["A","A","A","B","B","B"]
+    ["A","B","A","B","A","B"]
+    ["B","B","B","A","A","A"]
+    ...
+    And so on.
+    Example 3:
+    
+    Input: tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+    Output: 16
+    Explanation: 
+    One possible solution is
+    A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
+     
+    
+    Constraints:
+    
+    1 <= task.length <= 104
+    tasks[i] is upper-case English letter.
+    The integer n is in the range [0, 100].
+ * @author Ye Xianyi
+ * 2020/12/09
+ *
+ * Copy from:
+ * https://leetcode.com/problems/task-scheduler/solution/
+ */
+public class Solution {
+
+    public static int leastInterval(char[] tasks, int n) {
+        int[] frequencies = new int[26] ;
+        
+        for(char ch : tasks) {
+            frequencies[ch - 'A'] ++ ;
+        }
+        
+        // frequency from smaller to bigger
+        Arrays.sort(frequencies);
+        
+        // get the biggest frequencies num
+        int freqMax = frequencies[25] ; 
+        
+        int idleTime = (freqMax - 1) * n ;
+        
+        for(int i=24; i>=0 && idleTime >= 0; i--) {
+            idleTime = idleTime - Math.min(freqMax - 1, frequencies[i]) ;
+        }
+        
+        idleTime = Math.max(0, idleTime) ;
+        
+        return tasks.length + idleTime ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(leastInterval(new char[] {'A','A','A','B','B','B'}, 2)) ;
+        System.out.println(leastInterval(new char[] {'A','A','A','B','B','B'}, 0)) ;
+        System.out.println(leastInterval(new char[] {'A','A','A','A','A','A','B','C','D','E','F','G'}, 2)) ;
+    }
+
+}
